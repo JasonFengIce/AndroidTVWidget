c15443895dcd2bf4655aa9e5a36655897913b5f4
hailongqiu <356752238@qq.com>
2016-02-27 11:32:47 +0800

    code

--- AndroidTvWidget/src/com/open/androidtvwidget/MainActivity.java
+++ AndroidTvWidget/src/com/open/androidtvwidget/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends Activity {
 				mainUpView1.setFocusView(newFocus, oldFocus, 1.1f);
 				mainUpView1.setDrawUpRectEnabled(true);
 				if (newFocus != null) {
-					testTopDemo(newFocus);
+//					testTopDemo(newFocus);
 				}
 			}
 		});
