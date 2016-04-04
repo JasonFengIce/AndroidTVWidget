7eaf6e1f76b62819270c337868a7aa016c48dcc8
356752238@qq.com <356752238@qq.com>
2016-04-05 07:55:23 +0800

    no

--- AndroidTvWidget/src/com/open/androidtvwidget/view/ListViewTV.java
+++ AndroidTvWidget/src/com/open/androidtvwidget/view/ListViewTV.java
@@ -3,7 +3,6 @@ package com.open.androidtvwidget.view;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 
