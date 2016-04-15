9dfbf4b0bbbe8859c4b936bec936ba09f41875dc
hailongqiu <hailong.qiu@xgimi.com>
2016-04-15 15:43:41 +0800

    整理代码

--- demo/src/com/open/demo/DemoKeyBoardActivity.java
+++ demo/src/com/open/demo/DemoKeyBoardActivity.java
@@ -8,6 +8,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +18,7 @@ import android.widget.Toast;
  * top_bottom_move 是设置上下不循环.
  * left_right_move 默认为true,就像DEMO里一样，是左右循环的.
  * 具体看DEMO.
+ * 千万不要将 keyCode设置为 0.
  */
 public class DemoKeyBoardActivity extends Activity {
 
@@ -35,11 +38,12 @@ public class DemoKeyBoardActivity extends Activity {
 		skbContainer.setOnSoftKeyBoardListener(new SoftKeyBoardListener() {
 			@Override
 			public void onCommitText(SoftKey softKey) {
+				int keyCode = softKey.getKeyCode();
 				String keyLabel = softKey.getKeyLabel();
 				if (!TextUtils.isEmpty(keyLabel)) { // 输入文字.
 					input_tv.setText(input_tv.getText() + softKey.getKeyLabel());
 				} else { // 自定义按键，这些都是你自己在XML中设置的keycode.
-					int keyCode = softKey.getKeyCode();
+					keyCode = softKey.getKeyCode();
 					if (keyCode == KeyEvent.KEYCODE_DEL) {
 						String text = input_tv.getText().toString();
 						input_tv.setText(text.substring(0, text.length() - 1));
@@ -49,6 +53,19 @@ public class DemoKeyBoardActivity extends Activity {
 						Toast.makeText(getApplicationContext(), "回车", Toast.LENGTH_LONG).show();
 					}
 				}
+			}
+		});
+		// 
+		findViewById(R.id.en_btn).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				skbContainer.setSkbLayout(R.xml.sbd_qwerty);
+			}
+		});
+		findViewById(R.id.num_btn).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				skbContainer.setSkbLayout(R.xml.sbd_number);
 			}
 		});
 	}
