39ed7edff8e5a6e52ede876d7d50556a7794724d
hailongqiu <hailong.qiu@xgimi.com>
2016-05-17 11:04:00 +0800

    键盘添加设置取消高亮，修复崩溃问题

--- AndroidTvWidget/src/com/open/androidtvwidget/recycle/RecyclerViewTV.java
+++ AndroidTvWidget/src/com/open/androidtvwidget/recycle/RecyclerViewTV.java
@@ -1,20 +1,11 @@
 package com.open.androidtvwidget.recycle;
 
-import com.open.androidtvwidget.R;
-import com.open.androidtvwidget.utils.DisplayUtil;
 import com.open.androidtvwidget.view.WidgetTvViewBring;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Rect;
-import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 
 public class RecyclerViewTV extends RecyclerView {
 
@@ -34,7 +25,6 @@ public class RecyclerViewTV extends RecyclerView {
 	}
 
 	WidgetTvViewBring mWidgetTvViewBring;
-	Drawable mDrawable;
 
 	private void init(Context context) {
 		setDescendantFocusability(FOCUS_AFTER_DESCENDANTS);
@@ -43,8 +33,6 @@ public class RecyclerViewTV extends RecyclerView {
 		setOverScrollMode(View.OVER_SCROLL_NEVER);
 		setChildrenDrawingOrderEnabled(true);
 		mWidgetTvViewBring = new WidgetTvViewBring(this);
-		//
-		mDrawable = getResources().getDrawable(R.drawable.white_light_10);
 	}
 
 	@Override
@@ -52,38 +40,9 @@ public class RecyclerViewTV extends RecyclerView {
 		mWidgetTvViewBring.bringChildToFront(this, child);
 	}
 
-	// @Override
-	// protected int getChildDrawingOrder(int childCount, int i) {
-	// return mWidgetTvViewBring.getChildDrawingOrder(childCount, i);
-	// }
-
-//	@Override
-//	protected void dispatchDraw(Canvas canvas) {
-//		try {
-//			super.dispatchDraw(canvas);
-//			//
-//			Log.d("hailongqiu", "hailongqiu dispatchDraw");
-//			View v = getFocusedChild();
-//			Rect rect = new Rect();
-//			v.getGlobalVisibleRect(rect);
-//			Rect gridRect = new Rect();
-//			getGlobalVisibleRect(gridRect);
-//			rect.offset(-gridRect.left, -gridRect.top);
-//			mDrawable.setBounds(new Rect(DisplayUtil.dip2px(getContext(), rect.left),
-//					DisplayUtil.dip2px(getContext(), rect.top), DisplayUtil.dip2px(getContext(), rect.right),
-//					DisplayUtil.dip2px(getContext(), rect.bottom)));
-////			mDrawable.setBounds(gridRect);
-//			mDrawable.draw(canvas);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-
-	public Rect findLocationWithView(View view) {
-		ViewGroup root = (ViewGroup) getParent();
-		Rect rect = new Rect();
-		root.offsetDescendantRectToMyCoords(view, rect);
-		return rect;
+	@Override
+	protected int getChildDrawingOrder(int childCount, int i) {
+		return mWidgetTvViewBring.getChildDrawingOrder(childCount, i);
 	}
 
 }
