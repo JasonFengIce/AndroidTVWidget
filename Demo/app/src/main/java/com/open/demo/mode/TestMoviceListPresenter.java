bbc1c94f8353afe91015f598fed008e2a830a482
356752238@qq.com <356752238@qq.com>
2016-08-25 23:46:16 +0800

    完善leanback demo

--- Demo/app/src/main/java/com/open/demo/mode/TestMoviceListPresenter.java
+++ Demo/app/src/main/java/com/open/demo/mode/TestMoviceListPresenter.java
@@ -3,9 +3,10 @@ package com.open.demo.mode;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.TextView;
 
 import com.open.androidtvwidget.leanback.mode.DefualtListPresenter;
+import com.open.demo.R;
 
 /**
  * Created by hailongqiu on 2016/8/25.
@@ -13,15 +14,14 @@ import com.open.androidtvwidget.leanback.mode.DefualtListPresenter;
 public class TestMoviceListPresenter extends DefualtListPresenter {
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View headview = LayoutInflater.from(parent.getContext()).inflate(com.open.androidtvwidget.R.layout.list_menu_item_layout, parent, false);
-        Button btn = new Button(parent.getContext());
-        return new ViewHolder(btn);
+        View headview = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_recyclerview_view, parent, false);
+        return new ViewHolder(headview);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, int position) {
         Movie movie = ((Movie) getItem(position));
-        Button btn = ((Button) viewHolder.view);
-        btn.setText(movie.getTitle());
+        TextView textview = (TextView) viewHolder.view.findViewById(R.id.textView);
+        textview.setText(movie.getTitle());
     }
 }
