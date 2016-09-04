a7da04cbd986e29c9677bbfb2137203e255a53f9
356752238@qq.com <356752238@qq.com>
2016-09-05 00:24:19 +0800

    add json

---
+++ Demo/app/src/main/java/com/open/demo/network/mode/BaseJsonData.java
@@ -0,0 +1,10 @@
+package com.open.demo.network.mode;
+
+/**
+ * Json数据.
+ * Created by hailongqiu on 2016/9/5.
+ */
+public class BaseJsonData {
+    private static final int VIEW_PAGER_TYPE = 1; // ViewPager类型.
+    private int type;
+}
