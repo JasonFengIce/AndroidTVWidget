17ecd5db01246a26139dc34faebe4295ce8cbf5e
hailong.qiu <hailong.qiu@xgimi.com>
2016-09-13 21:24:01 +0800

    json load xml

--- Demo/androidtvwidget/src/main/java/com/open/androidtvwidget/utils/IdentifierUtuils.java
+++ Demo/androidtvwidget/src/main/java/com/open/androidtvwidget/utils/IdentifierUtuils.java
@@ -8,6 +8,30 @@ import android.content.Context;
  */
 public class IdentifierUtuils {
 
+    public static float getIdentifierWidthDimen(Context context, String name) {
+        try {
+            if (!name.startsWith("w_"))
+                name = "w_" + name;
+            int id = getIdentifierDimen(context, name);
+            return context.getResources().getDimension(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public static float getIdentifierHeightDimen(Context context, String name) {
+        try {
+            if (!name.startsWith("h_"))
+                name = "h_" + name;
+            int id = getIdentifierDimen(context, name);
+            return context.getResources().getDimension(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
     public static int getIdentifierDimen(Context context, String name) {
         int resid = context.getResources().getIdentifier(name, "dimen", context.getPackageName());
         return resid;
